2fd5830e65033d7520b3197d1e9fdad02ecbf49a
ankur-vasta <ankur.vasta@quantasis.com>
2025-02-24 11:31:12 +0530

    Added connection & models

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+const Role = db.role;
+
+db.mongoose
+  .connect(
+    `mongodb+srv://ankurvasta123:${process.env.PASSWORD}@cluster0.1f6xg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
+  )
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch((err) => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+async function initial() {
+  try {
+    const count = await Role.estimatedDocumentCount();
+
+    if (count === 0) {
+      await new Role({ name: "user" }).save();
+      console.log("added 'user' to roles collection");
+
+      await new Role({ name: "moderator" }).save();
+      console.log("added 'moderator' to roles collection");
+
+      await new Role({ name: "admin" }).save();
+      console.log("added 'admin' to roles collection");
+    }
+  } catch (err) {
+    console.error("Error initializing roles:", err);
+  }
+}
